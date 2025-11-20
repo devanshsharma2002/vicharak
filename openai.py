8bd581ffa75bc70e8e3f5521cdf1573dc2c3437a
Devansh Sharma <devanshsharma969@gmail.com>
2025-11-21 00:50:46 +0530

    initial commit

--- openai.py
+++ openai.py
@@ -1,8 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(
-  api_key="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"
-)
+client = OpenAI()
 
 response = client.responses.create(
   model="gpt-5-nano",
